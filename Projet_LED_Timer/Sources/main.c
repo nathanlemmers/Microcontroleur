d41df0b5620087e1c5895ceac06f4a7e17ba493e
Nicolas Rigal <nrigal@insa-toulouse.fr>
2022-10-21 15:18:57 +0200

    PWM finie !

--- Projet_LED_Timer/Sources/main.c
+++ Projet_LED_Timer/Sources/main.c
@@ -9,19 +9,34 @@ void callback (void)
 {
 	MyGPIO_Toggle(Led_Int.GPIO, Led_Int.GPIO_Pin) ;
 }
+
+void testPWM ()
+{
+	MyTimer_Struct_TypeDef TimerSet = {TIM2, 5999, 5999} ;
+	
+	MyTimer_Base_Init(&TimerSet);
+	
+	MyTimer_PWM(TimerSet.Timer, 1);
+	MyTimer_PWM_Cycle(TimerSet.Timer, 1, 0.2);
+	MyTimer_PWM_ToGPIO(TimerSet.Timer, 1) ;
+	MyTimer_Base_Start(TimerSet.Timer);
+}
 	
 int main (void)
 {
-	MyTimer_Struct_TypeDef Timer = {TIM1, 5999, 5999} ;
-	MyTimer_Base_Init(&Timer);
-	MyTimer_Base_Start(Timer.Timer);
-	MyTimer_ActiveIT(Timer.Timer, 7, &callback);
+	MyTimer_Struct_TypeDef TimerSet = {TIM1, 5999, 5999} ;
+	MyTimer_Base_Init(&TimerSet);
+	MyTimer_Base_Start(TimerSet.Timer);
+	MyTimer_ActiveIT(TimerSet.Timer, 7, &callback);
 	
 	// Configuration led2
 	Led_Int.GPIO = GPIOA ;
 	Led_Int.GPIO_Conf = Out_Ppull ;
 	Led_Int.GPIO_Pin = 5 ;
 	MyGPIO_Init (&Led_Int) ;
+	
+	testPWM();
+	
 	while (1) {
 
 	}
